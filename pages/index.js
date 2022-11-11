6bc4b47bb30134a11cb85128c3a6432874cf8f4f
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-10 22:05:17 -0300

    Implementado DarkMode

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import Menu from "../src/components/Menu";
-import { StyledTimeline } from "../src/components/Timeline"
+import { StyledTimeline } from "../src/components/timeline"
 
 function HomePage() {
   const [valorDoFiltro, setValorDoFiltro] = React.useState("");
